14ae51da55a8c886cc67b339e3b50b8b8c284b15
T-Kisaku <kisakutakashi0226@gmail.com>
2023-11-19 11:53:00 +0900

    commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import {PUBLIC_GOOGLE_LOGIN_URL} from '$env/static/public'
+    import './login.scss'
+</script>
+<div class="container">
+    <!-- <input type="text" placeholder="email"> -->
+    <button class="login-with-google-btn">
+        <a href={PUBLIC_GOOGLE_LOGIN_URL}>LOGIN WITH GOOGLE</a>
+    </button>
+</div>
