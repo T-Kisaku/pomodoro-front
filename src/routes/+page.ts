f65af815f0846ad4f55e1e3d1aaf0f2fba0752a4
T-Kisaku <kisakutakashi0226@gmail.com>
2023-12-10 19:15:16 +0900

    Update +page.ts

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { load_Profile } from '$houdini'
 import type { PageLoad } from './$houdini'
 
 export const load: PageLoad = async (event) => {
-    event.setHeaders({ 'cache-control': 'no-cache' })
+    event.setHeaders({ 'Cache-Control': 'no-cache' })
     return {
         ...(await load_Profile({ event })),
     }
