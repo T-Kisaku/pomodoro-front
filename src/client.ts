814d46da7363740f8c6a5bca69b9700b02f01d58
T-Kisaku <kisakutakashi0226@gmail.com>
2023-12-10 10:47:14 +0900

    Add a env variable

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,4 @@
 import { HoudiniClient } from '$houdini';
+import {PUBLIC_GRAPHQL_URL} from '$env/static/public'
 
-export default new HoudiniClient({
-    url: 'http://localhost:4000/graphql'
-
-    // uncomment this to configure the network call (for things like authentication)
-    // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
-    // fetchParams({ session }) {
-    //     return {
-    //         headers: {
-    //             Authentication: `Bearer ${session.token}`,
-    //         }
-    //     }
-    // }
-})
+export default new HoudiniClient({url: PUBLIC_GRAPHQL_URL})
